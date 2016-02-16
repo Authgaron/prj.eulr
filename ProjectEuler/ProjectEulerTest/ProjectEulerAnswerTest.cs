9c10c152b033ed5bffc12b81f5f1ee86c98c40a6
kevin.gray <kevin.gray@entelect.co.za>
2016-02-16 23:35:02 +0200

    Created basic class structure, including Prime helper class, Problem1 class and a test project containing tests for both the answers and the sample data.

---
+++ ProjectEuler/ProjectEulerTest/ProjectEulerAnswerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectEulerTest
+{
+    [TestClass]
+    public class ProjectEulerAnswerTest
+    {
+        [TestMethod]
+        public void Problem1()
+        {
+        }
+    }
+}
---
+++ ProjectEuler/ProjectEulerTest/ProjectEulerSampleTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectEulerTest
+{
+    [TestClass]
+    public class ProjectEulerSampleTest
+    {
+        [TestMethod]
+        public void Problem1()
+        {
+        }
+    }
+}
